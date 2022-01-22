506c7db53adba70c1b791fd611a557d1a895f652
andrew-craig <andrcra@gmail.com>
2022-01-22 14:26:20 -0800

    add tag button from Recipes

---
+++ tag-button.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function TagButton(props) {
+    var buttonClass = ''
+
+    if (props.filter.includes(props.name)) {
+        buttonClass = 'tag-button tag-button-active'
+    } else {
+        buttonClass = 'tag-button'
+    }
+    console.log(buttonClass)
+
+    return (
+        <button 
+            className={buttonClass}
+            onClick={() => { props.handleClick(props.name) }}>
+            {props.name}
+        </button>
+    )
+}
