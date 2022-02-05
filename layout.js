26fec002a2944f302107e1070b5056f2341766a9
andrew-craig <andrcra@gmail.com>
2022-02-04 20:25:30 -0800

    switch to Inter

--- layout.js
+++ layout.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { MDXProvider } from "@mdx-js/react"
 import Location from "./location"
-import "@fontsource/open-sans/400.css"
-import "@fontsource/open-sans/400-italic.css"
-import "@fontsource/open-sans/700.css"
-import "@fontsource/open-sans/700-italic.css"
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/700.css"
 import './layout.css'
 
 const shortcodes = { Location }
