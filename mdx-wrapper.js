7241f891821c5577879cb1046761272ee7bdb3a1
andrew-craig <andrcra@gmail.com>
2023-12-29 21:23:37 +1100

    update mdx wrapper

--- mdx-wrapper.js
+++ mdx-wrapper.js
@@ -3,9 +3,14 @@ import { MDXProvider } from "@mdx-js/react"
 import Location from "./location"
 import lbImage from "./lbImage"
 
-const shortcodes = { Location }
 
-// components={shortcodes}
+const components = {
+  Location: Location,
+  img: lbImage
+}
+
+//,div: lbImage
+
 
 // add to the components something like
 // const lbImg = props => <h1 style={{ color: `tomato` }} {...props} />
@@ -14,9 +19,11 @@ const shortcodes = { Location }
 //  img: lbImg,
 //}
 
+//div: lbImage, 
+
 export default function MDXWrapper({ children }) {
 
   return (
-      <MDXProvider components={{div: lbImage}}>{children}</MDXProvider>
+      <MDXProvider components={components}>{children}</MDXProvider>
   )
 }
