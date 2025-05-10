6441344bc032aa9a6dd99ba2177d33157b14cd2b
andrew-craig <andrcra@gmail.com>
2025-05-10 22:42:09 +1000

    Add mono font for recipe timer

--- layout.js
+++ layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "@fontsource/inter/400.css"
 import "@fontsource/inter/500.css"
 import "@fontsource/inter/700.css"
+import "@fontsource/roboto-mono/400.css"
 import './layout.scss'
 
 export default function Layout({ mode, children }) {
